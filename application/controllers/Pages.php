4025a71b73eb0a7ade7534332132d50f11c84668
Vitaly <vitalykk1983@gmail.com>
2019-02-19 14:42:14 +0200

    Add style in header.php

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -3,7 +3,7 @@
 
 class Pages extends CI_Controller
 {
-    public function view($page = 'about')
+    public function view($page = 'home')
     {
         if (!file_exists(APPPATH . 'views/pages/' . $page . '.php')) {
             // Whoops, we don't have a page for that!
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,7 @@
-<html>
+<html lang="en">
 <head>
     <title>CodeIgniter Tutorial</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
 </head>
 <body>
 <h1><?php echo $title; ?></h1>
